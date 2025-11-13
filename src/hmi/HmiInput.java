59ec76cd6ac625c83d022f881603f3cf252b0251
andrefuchs3 <andre_fuchs_3@web.de>
2025-11-13 18:15:36 +0100

    Update HmiInput.java

--- src/hmi/HmiInput.java
+++ src/hmi/HmiInput.java
@@ -1,2 +1,18 @@
 package hmi;
-public class HmiInput { /* TODO */ }
+
+import core.CooktopController;
+import util.Types.ZoneID;
+
+/** Eingabe-Fassade: Methoden, die in der UI (Touch) auslösbar wären */
+public class HmiInput {
+    private final CooktopController controller;
+
+    public HmiInput(CooktopController controller) {
+        this.controller = controller;
+    }
+
+    public void selectZone(ZoneID z, boolean active) { controller.setZoneActive(z, active); }
+    public void increasePower(ZoneID z)              { controller.increasePower(z); }
+    public void decreasePower(ZoneID z)              { controller.decreasePower(z); }
+    public void toggleChildLock()                    { controller.toggleChildLock(); }
+}
