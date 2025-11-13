94e3989d4993aa72b3a9ab1b305a03f113487f7f
andrefuchs3 <andre_fuchs_3@web.de>
2025-11-13 18:15:22 +0100

    Update HmiOutput.java

--- src/hmi/HmiOutput.java
+++ src/hmi/HmiOutput.java
@@ -1,2 +1,22 @@
 package hmi;
-public class HmiOutput { /* TODO */ }
+
+import util.Types.ZoneID;
+
+/** Vereinfachte Anzeige-Ausgabe (Platzhalter: Konsole/Log) */
+public class HmiOutput {
+    public void showActiveZone(ZoneID zone, boolean active) {
+        System.out.printf("[HMI] Zone %s %s%n", zone, active ? "AKTIV" : "INAKTIV");
+    }
+
+    public void showPowerLevel(ZoneID zone, int level) {
+        System.out.printf("[HMI] Zone %s -> Leistungsstufe %d%n", zone, level);
+    }
+
+    public void showLock(boolean locked) {
+        System.out.printf("[HMI] Kindersicherung: %s%n", locked ? "AKTIV" : "AUS");
+    }
+
+    public void showError(String message) {
+        System.out.printf("[HMI] FEHLER: %s%n", message);
+    }
+}
