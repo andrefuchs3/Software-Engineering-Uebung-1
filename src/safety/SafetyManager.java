ed1c471153348a99c173b895ed261e548b9cc27c
andrefuchs3 <andre_fuchs_3@web.de>
2025-11-13 18:13:27 +0100

    Update SafetyManager.java

--- src/safety/SafetyManager.java
+++ src/safety/SafetyManager.java
@@ -1,17 +1,24 @@
 package safety;
 
+/** Kindersicherung als Singleton (eine Instanz im ganzen System) */
 public final class SafetyManager {
-    private static SafetyManager instance;
+    private static volatile SafetyManager instance;
     private boolean locked;
 
-    private SafetyManager() { this.locked = false; }
+    private SafetyManager() {
+        this.locked = false;
+    }
 
-    public static synchronized SafetyManager getInstance() {
-        if (instance == null) instance = new SafetyManager();
+    public static SafetyManager getInstance() {
+        if (instance == null) {
+            synchronized (SafetyManager.class) {
+                if (instance == null) instance = new SafetyManager();
+            }
+        }
         return instance;
     }
 
     public boolean isLocked() { return locked; }
-    public void lockInput() { locked = true; }
+    public void lockInput()   { locked = true; }
     public void unlockInput() { locked = false; }
 }
